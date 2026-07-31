16ae2352981b4cef6bff100f4642fae2007c4c0d
Chukwuma-oz <ozurumbachukwuma1@outlook.com>
2026-07-31 03:08:21 -0700

    Update API_BASE to production ALB DNS

--- frontend/config.js
+++ frontend/config.js
@@ -4,5 +4,5 @@
 //         custom domain in front of it, e.g. "http://naijacart-alb-123.af-south-1.elb.amazonaws.com"
 //         then re-upload this file to the S3 frontend bucket and invalidate CloudFront.
 window.NAIJACART_CONFIG = {
-  API_BASE: 'http://localhost:8080',
+  API_BASE: 'http://naijac-LoadB-yW9o9GwkpczZ-2143192714.us-east-1.elb.amazonaws.com',
 };
